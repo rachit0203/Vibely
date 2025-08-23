ba2a9270c082a286d68c857ba81440cbb50d5a0c
rachit0203 <159680329+rachit0203@users.noreply.github.com>
2025-08-23 22:43:45 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
 
 export const axiosInstance = axios.create({
-    baseURL: BASE_URL,
+    baseURL: "https://vibely-2-backend.onrender.com/api",
     withCredentials: true, // send cookies with the request 
 });
